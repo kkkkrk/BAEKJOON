ab9b29fa18518b3ef383d049b9c9e8a617f37d3a
kkkkrk <62200028+kkkkrk@users.noreply.github.com>
2024-10-08 21:25:03 +0900

    Add files via upload

---
+++ 11651.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool cmp(pair<int,int>&x, pair<int, int>&y){
+    if(x.second == y.second) return x.first < y.first;
+    return x.second < y.second;
+}
+int main()
+{
+    vector<pair<int,int>> v;
+    int N;
+    cin >> N;
+    for(int i = 0;i < N;i++){
+        int a,b;
+        cin >> a >> b;
+        v.push_back(make_pair(a,b));
+    }
+    sort(v.begin(), v.end(), cmp);
+    for(auto n: v){
+        cout << n.first << " " << n.second << "\n";
+    }
+    
+
+    return 0;
+}
