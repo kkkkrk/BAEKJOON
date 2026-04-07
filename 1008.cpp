b1024e6eba2fdb7b1ea025211618b69182ad0d3c
kkkkrk <62200028+kkkkrk@users.noreply.github.com>
2026-04-07 20:10:33 +0900

    Add basic division program in 1008.cpp

---
+++ 1008.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // cout.fixed;
+    cout.precision(12);
+    double a,b;
+    cin >> a >> b;
+    cout << a / b;
+
+    return 0;
+}
